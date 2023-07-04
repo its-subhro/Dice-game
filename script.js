9c400df07fd16df8c183523312eee053704205f4
Subhrojyoti Sen <136054780+its-subhro@users.noreply.github.com>
2023-07-04 23:08:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
+var name_1;
+var name_2;
 
+var play_1;
+var play_2;
 
-    var name_1 = prompt("Enter player 1 name:");
-    var name_2 = prompt("Enter player 2 name:"); 
+document.querySelector(".start").addEventListener("click",function takeInput() {
+
+     name_1 = prompt("Enter player 1 name:");
+     name_2 = prompt("Enter player 2 name:"); 
+
     document.querySelector(".player1").textContent = name_1;
-    document.querySelector(".player2").textContent = name_2;            
-    
-function rollDice() {
+    document.querySelector(".player2").textContent = name_2;
+    document.querySelector(".start").style.display = "none";
+    document.querySelector(".play-button").style.display = "block";      
+});
 
-    var play_1 = Math.floor(Math.random()*6)+1;
-    var play_2 = Math.floor(Math.random()*6)+1;
+document.querySelector(".play-button").addEventListener("click",function rollDice() {
+
+     play_1 = Math.floor(Math.random()*6)+1;
+     play_2 = Math.floor(Math.random()*6)+1;
 
     if (play_1 > play_2) {
 
+        document.querySelector(".winner-name").textContent = "Winner is "+name_1;
         document.querySelector(".dice1").src = "dice"+play_1+".png";
         document.querySelector(".dice2").src = "dice"+play_2+".png";
-        document.querySelector(".winner-name").textContent = "Winner is "+name_1;
     } 
     else if (play_2 > play_1){
         
+        document.querySelector(".winner-name").textContent = "Winner is "+name_2;
         document.querySelector(".dice1").src = "dice"+play_1+".png";
         document.querySelector(".dice2").src = "dice"+play_2+".png";
-        document.querySelector(".winner-name").textContent = "Winner is "+name_2;
         
     }
     else if (play_1===play_2) {
@@ -28,4 +38,4 @@ function rollDice() {
         document.querySelector(".dice1").src = "dice"+play_1+".png";
         document.querySelector(".dice2").src = "dice"+play_2+".png";
     }
-}
+});
